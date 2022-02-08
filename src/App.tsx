610839296594e2cd4fab2d3b95ef6accf2cc1f63
Amanda Manduca <amandavmanduca@hotmail.com>
2022-02-08 01:00:58 -0300

    :arrow_up: Updating dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RoutesSection from './routes';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { AdminTemplate } from './atomic/template/AdminTemplate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AdminTemplate>
+      <RoutesSection />
+      <ToastContainer hideProgressBar={true} theme={'dark'} />
+    </AdminTemplate>
   );
 }
 
